43bfad8d5560d9f3138a8a3b1f72e2ff06b457ee
Gabonozaur <gig.gabriel97@gmail.com>
2023-02-17 00:10:56 +0200

    add components library

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,11 @@
-import type { AppProps } from 'next/app'
+import { ChakraProvider, Text } from "@chakra-ui/react";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider>
+      <Text color={{ base: "green.500", sm: "blue.500" }}>Marcel is there</Text>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
